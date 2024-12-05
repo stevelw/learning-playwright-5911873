fdabc2521cc596cc534284e61b3bb928f4035557
stevelw <steve@stevelw.co.uk>
2024-12-05 11:49:49 +0000

    add example tests

---
+++ tests/home.spec.ts
@@ -0,0 +1,18 @@
+import { test, expect } from '@playwright/test';
+
+test("Home page", async ({ page }) => {
+  await page.goto("https://practicesoftwaretesting.com/");
+
+  // Ensure signin link is present
+  await expect(page.getByTestId("nav-sign-in")).toHaveText("Sign in");
+  
+  // Check the title of the page
+  await expect(page).toHaveTitle("Practice Software Testing - Toolshop - v5.0");
+
+  // Check the count of items displayed
+  const productGrid = page.locator(".col-md-9");
+  await expect(productGrid.getByRole("link")).toHaveCount(9);
+  expect( await productGrid.getByRole("link").count()).toBe(9);
+
+  // Search for Thor Hammer and check the result
+})

0d3f0cbf01b24aecd0f610518199d9d070bb3732
stevelw <steve@stevelw.co.uk>
2024-12-09 11:09:28 +0000

    Fix login test due to user blacklisting

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -5,11 +5,11 @@ test("login test", async ({ page }) => {
   await page.locator('[data-test="nav-sign-in"]').click();
   await page
     .locator('[data-test="email"]')
-    .fill("customer@practicesoftwaretesting.com");
+    .fill("customer2@practicesoftwaretesting.com");
   await page.locator('[data-test="password"]').fill("welcome01");
   await page.locator('[data-test="login-submit"]').click();
   await expect(page.locator('[data-test="nav-menu"]')).toContainText(
-    "Jane Doe"
+    "Jack Howe"
   );
   await expect(page.locator('[data-test="page-title"]')).toContainText(
     "My account"
